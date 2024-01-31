ed20c4d05bb5a88ea8e67223acbc6f167b0346a8
Alexandru Aciobanitei <i444807@fhict.nl>
2024-01-31 02:31:18 +0100

    fixed final

--- phoneShake.js
+++ phoneShake.js
@@ -1,22 +1,22 @@
 //
 
 
-var myShakeEvent = new shake({
-    threshold: 15, // optional shake strength threshold
-    timeout: 1000 // optional, determines the frequency of event generation
-});
-console.log('shake')
-myShakeEvent.start();
+// var myShakeEvent = new shake({
+//     threshold: 15, // optional shake strength threshold
+//     timeout: 1000 // optional, determines the frequency of event generation
+// });
+// console.log('shake')
+// myShakeEvent.start();
 
-window.addEventListener('shake', shakeEventDidOccur, false);
+// window.addEventListener('shake', shakeEventDidOccur, false);
 
-//function to call when shake occurs
-function shakeEventDidOccur () {
+// //function to call when shake occurs
+// function shakeEventDidOccur () {
 
-    //put your own code here etc.
-    alert('shake!');
+//     //put your own code here etc.
+//     alert('shake!');
 
-    // Redirect to stestimonial.html
-    window.location.href = 'stestimonial.html';
-    myShakeEvent.stop();
-}
+//     // Redirect to stestimonial.html
+//     window.location.href = 'stestimonial.html';
+//     myShakeEvent.stop();
+// }
